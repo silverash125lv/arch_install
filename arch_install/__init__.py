5688099c60622b5a15244e4ae1187a411d8d4a85
ulicharm122lv <rasracapur122lv@gmail.com>
2021-08-01 19:14:48 -0300

    add função: install_gui()

--- arch_install/__init__.py
+++ arch_install/__init__.py
@@ -1,4 +1,5 @@
 import install_uefi as inu
+import gnome as gn
 
 
 def install():
@@ -15,10 +16,25 @@ def install():
     print('depois saia do chroot e antes de reiniciar use o comando "umount -a"')
 
 
+def install_gui():
+    h = "paru"
+
+    gn.sync_clock()
+    gn.firewall_conf()
+    gn.install_aur_helper(h)
+    gn.install_aur(h)
+    gn.install_packages()
+    gn.flatpak_install()
+    gn.reboot()
+
+
 resp = ""
 print("O deseja fazer?")
 print("1 - Instalar")
+print("2 - Instalar Ambiente Gráfico")
 resp = int(input())
 
 if resp == 1:
     install()
+elif resp == 2:
+    install_gui()
